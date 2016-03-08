8342ba11e8d204cd086e7df267b8df8c79909fd9
jiyingxin <736907613@qq.com>
2016-03-08 10:37:51 +0800

    修改了导出的图片命名: 讲真,累死我了. 一个个改的

---
+++ 源码/GameLive/TRProject/View/栏目/TRCategoriesCell.h
@@ -0,0 +1,15 @@
+//
+//  TRCategoriesCell.h
+//  TRProject
+//
+//  Created by jiyingxin on 16/3/8.
+//  Copyright © 2016年 Tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRCategoriesCell : UICollectionViewCell
+
+
+
+@end
---
+++ 源码/GameLive/TRProject/View/栏目/TRCategoriesCell.m
@@ -0,0 +1,13 @@
+//
+//  TRCategoriesCell.m
+//  TRProject
+//
+//  Created by jiyingxin on 16/3/8.
+//  Copyright © 2016年 Tarena. All rights reserved.
+//
+
+#import "TRCategoriesCell.h"
+
+@implementation TRCategoriesCell
+
+@end
